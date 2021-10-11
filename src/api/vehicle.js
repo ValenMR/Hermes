481420f291f3ce71dc8af6b8624a871c49b21310
Mateo <zaguirrez05@gmail.com>
2021-10-10 21:57:46 -0500

    :sparkles: view vehicle companies

---
+++ src/api/vehicle.js
@@ -0,0 +1,85 @@
+function getVehicleCompanies(driveName, origin, destination) {
+    // peticion al back
+    vehicleCompanies = [
+        {
+            "name": "UdeA",
+            "driveName": "Valentina Cadena",
+            "route": [
+                "Medellin - cali",
+                "Bogota - cali",
+            ],
+        },
+        {
+            "name": "UdeA",
+            "driveName": "Valentina Muñoz",
+            "route": [
+                "Bogota - cali",
+                "Bogota - cali",
+            ],
+        },
+        {
+            "name": "Pepe",
+            "driveName": "Mateo Aguirre 3",
+            "route": [
+                "Medellin - cali",
+                "Bogota - cali",
+                "Bogota - cali",
+            ]
+        },
+        {
+            "name": "Pepe",
+            "driveName": "Mateo Aguirre 2",
+            "route": [
+                "Medellin - cali",
+                "Bogota - cali",
+                "Bogota - cali",
+            ]
+        },
+        {
+            "name": "Pepe",
+            "driveName": "Mateo Aguirre 1",
+            "route": [
+                "Medellin - cali",
+                "Bogota - cali",
+                "Bogota - cali",
+            ]
+        },
+        {
+            "name": "Pepe",
+            "driveName": "Mateo Aguirre 1",
+            "route": [
+                "Medellin - cali",
+                "Bogota - cali",
+                "Bogota - cali",
+            ]
+        },
+        {
+            "name": "Pepe",
+            "driveName": "Mateo Aguirre 1",
+            "route": [
+                "Medellin - cali",
+                "Bogota - cali",
+                "Bogota - cali",
+            ]
+        },
+        {
+            "name": "Pepe",
+            "driveName": "Mateo Aguirre 1",
+            "route": [
+                "Medellin - cali",
+                "Bogota - cali",
+                "Bogota - cali",
+            ]
+        },
+        {
+            "name": "Pepe",
+            "driveName": "Mateo Aguirre 1",
+            "route": [
+                "Medellin - cali",
+                "Bogota - cali",
+                "Bogota - cali",
+            ]
+        }
+    ];
+    return vehicleCompanies
+}
