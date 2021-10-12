0350efdafc8514ba037154c219b58abe6d4e665d
Valen MR <valentinamunoz1010@gmail.com>
2021-10-11 22:11:52 -0500

    :sparkles: Routes view

---
+++ src/api/route.js
@@ -0,0 +1,94 @@
+function getRoutes(price, origin, destination) {
+    // peticion al back
+    routes = [
+        {
+            "Company": "UdeA",
+            "Driver": "Valentina Cadena",
+            "Origin": "Medellín",
+            "Destination": "Sopetrán",
+            "Distance": 50.2,
+            "Price": 50000
+        },
+        {
+            "Company": "UdeA",
+            "Driver": "Valentina Cadena",
+            "Origin": "Medellín",
+            "Destination": "Sopetrán",
+            "Distance": 50.2,
+            "Price": 50000
+        },
+        {
+            "Company": "UdeA",
+            "Driver": "Valentina Cadena",
+            "Origin": "Medellín",
+            "Destination": "Sopetrán",
+            "Distance": 50.2,
+            "Price": 50000
+        },
+        {
+            "Company": "UdeA",
+            "Driver": "Valentina Cadena",
+            "Origin": "Medellín",
+            "Destination": "Sopetrán",
+            "Distance": 50.2,
+            "Price": 50000
+        },
+        {
+            "Company": "UdeA",
+            "Driver": "Valentina Cadena1",
+            "Origin": "Medellín",
+            "Destination": "Sopetrán",
+            "Distance": 50.2,
+            "Price": 50000
+        },
+        {
+            "Company": "UdeA",
+            "Driver": "Valentina Cadena1",
+            "Origin": "Medellín",
+            "Destination": "Sopetrán",
+            "Distance": 50.2,
+            "Price": 50000
+        },
+        {
+            "Company": "UdeA",
+            "Driver": "Valentina Cadena1",
+            "Origin": "Medellín",
+            "Destination": "Sopetrán",
+            "Distance": 50.2,
+            "Price": 50000
+        },
+        {
+            "Company": "UdeA",
+            "Driver": "Valentina Cadena1",
+            "Origin": "Medellín",
+            "Destination": "Sopetrán",
+            "Distance": 50.2,
+            "Price": 50000
+        },
+        {
+            "Company": "UdeA",
+            "Driver": "Valentina Cadena1",
+            "Origin": "Medellín",
+            "Destination": "Sopetrán",
+            "Distance": 50.2,
+            "Price": 50000
+        },
+        {
+            "Company": "UdeA",
+            "Driver": "Valentina Cadena1",
+            "Origin": "Medellín",
+            "Destination": "Sopetrán",
+            "Distance": 50.2,
+            "Price": 50000
+        },
+        {
+            "Company": "UdeA",
+            "Driver": "Valentina Cadena100",
+            "Origin": "Medellín",
+            "Destination": "Sopetrán",
+            "Distance": 50.2,
+            "Price": 50000
+        }        
+    ];
+    return routes
+}
